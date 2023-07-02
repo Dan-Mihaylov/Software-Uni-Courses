8b729c8fe928a8e3c0dacf121983a2355a4d5222
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-07-02 15:48:17 +0100

    More exercises Solved
    Solved encapsulation exercises

---
+++ Python OOP/Encapsulation/04_email_validator.py
@@ -0,0 +1,29 @@
+class EmailValidator:
+
+    def __init__(self, min_length: int, mails: list, domains: list):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def validate(self, email: str):
+        name, rest = email.split("@")
+        mail, domain = rest.split(".")
+        return all([self.__is_name_valid(name), self.__is_mail_valid(mail), self.__is_domain_valid(domain)])
+
+    def __is_name_valid(self, name: str):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail: str):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain: str):
+        return domain in self.domains
+
+
+# mails = ["gmail", "softuni"]
+# domains = ["com", "bg"]
+# email_validator = EmailValidator(6, mails, domains)
+# print(email_validator.validate("pe77er@gmail.com"))
+# print(email_validator.validate("georgios@gmail.net"))
+# print(email_validator.validate("stamatito@abv.net"))
+# print(email_validator.validate("abv@softuni.bg"))
