a0915f848c9c115cc5ccc02fefac15a91461c792
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-09-13 21:01:57 +0100

    More Basic CRUD exercises solved
    Solved more of the exercises

--- PostgreSQL/Basic CRUD Exercises/basic_crud_all_exercises.sql
+++ PostgreSQL/Basic CRUD Exercises/basic_crud_all_exercises.sql
@@ -75,28 +75,79 @@ OR number < 1000;
 
 -- 10. Set of Values
 
+SELECT
+    employee_id, project_id
+    FROM employee_projects
+    WHERE employee_id IN (200, 250)
+    AND project_id NOT IN (50, 100);
 
 -- 11. Compare Character Values
 
+SELECT
+	name, start_date
+	FROM projects
+	WHERE name IN ('Mountain', 'Road', 'Touring')
+	LIMIT 20;
+
+-- 12. Salary
+
+SELECT
+	first_name || ' ' || last_name AS "Full Name",
+	job_title,
+	salary
+	FROM employees
+	WHERE salary in (12500, 14000, 23600, 25000)
+	ORDER BY salary DESC;
 
 
--- 12. Salary13. Missing Value
+-- 13. Missing Value
 
+SELECT
+	id, first_name, last_name
+	FROM employees
+	WHERE middle_name IS NULL
+	LIMIT 3;
 
 -- 14. INSERT Departments
 
+INSERT INTO departments
+	(id, department, manager_id)
+	VALUES
+	(10, 'Finance', 3),
+	(11, 'Information Services', 42),
+	(12, 'Document Control', 90),
+	(13, 'Quality Assurance', 274),
+	(14, 'Facilities and Maintenance', 218),
+	(15, 'Shipping and Receiving', 85),
+	(16, 'Executive', 109);
 
 -- 15. New Table
 
+CREATE TABLE company_chart AS
+	SELECT
+		first_name || ' ' || last_name AS "Full Name",
+		job_title AS "Job Title",
+		department_id AS "Department ID",
+		manager_id AS "Manager Id"
+		From employees;
 
 -- 16. Update the Project End Date
 
+UPDATE projects
+	SET end_date = start_date + INTERVAL '5 MONTHS'
+	WHERE end_date IS NULL;
 
 -- 17. Award Employees with Experience
 
+UPDATE employees
+	SET
+	salary = salary + 1500,
+	job_title = 'Senior' || ' ' || job_title
+	WHERE hire_date BETWEEN '1998-01-01' AND '2000-01-05';
 
 -- 18. Delete Addresses
 
+DELETE FROM addresses WHERE city_id IN (5, 17, 20, 30);
 
 -- 19. Create a View
 
