4ef88fbe8376b11aa98263a88631fc4383c38887
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-09-20 16:15:08 +0100

    Solved Exercises From Data Aggregation

---
+++ PostgreSQL/04_Data Aggregation Lab/all_exercises.sql
@@ -0,0 +1,106 @@
+-- 01. Departments Info by ID
+
+SELECT
+    department_id,
+    COUNT(id) AS "employee_count"
+FROM
+    employees
+GROUP BY
+    department_id
+ORDER BY
+    department_id;
+
+-- 02. Departments Info by Salary
+
+SELECT
+    department_id,
+    COUNT(salary) AS "employee_count"
+FROM
+    employees
+GROUP BY
+    department_id
+ORDER BY
+    department_id;
+
+-- 03. Sum Salaries per Department
+
+SELECT
+    department_id,
+    SUM(salary) AS "total_salaries"
+FROM
+    employees
+GROUP BY
+    department_id
+ORDER BY
+    department_id;
+
+-- 04. Maximum Salary
+
+SELECT
+    department_id,
+    MAX(salary) AS "max_salary"
+FROM
+    employees
+GROUP BY
+    department_id
+ORDER BY
+    department_id;
+
+-- 05. Minimum Salary
+
+SELECT
+    department_id,
+    MIN(salary) AS "min_salary"
+FROM
+    employees
+GROUP BY
+    department_id
+ORDER BY
+    department_id;
+
+-- 06. Average Salary
+
+SELECT
+    department_id,
+    AVG(salary) AS "avg_salary"
+FROM
+    employees
+GROUP BY
+    department_id
+ORDER BY
+    department_id;
+
+-- 07. Filter Total Salaries
+-- The HAVING Clause has to have an aggregate function (Question 1: Why does it have to be an aggregate function
+-- and cannot just take the result from the generated column that had ran the aggregate function to generate the res?)
+
+SELECT
+    department_id,
+    SUM(salary) AS "Total Salary"
+FROM
+    employees
+GROUP BY
+    department_id
+HAVING
+    SUM(salary) < 4200
+ORDER BY
+    department_id;
+
+--08. Department Names
+
+SELECT
+    id,
+    first_name,
+    last_name,
+    TO_CHAR(salary, '9999999.99') AS salary,
+    department_id,
+    CASE
+        WHEN department_id = 1 THEN 'Management'
+        WHEN department_id = 2 THEN 'Kitchen Staff'
+        WHEN department_id = 3 THEN 'Service Staff'
+        ELSE 'Other'
+    END AS department_name
+FROM
+    employees
+ORDER BY
+    id;
