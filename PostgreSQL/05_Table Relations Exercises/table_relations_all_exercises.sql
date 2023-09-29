c5e7a8b33823efcbcef883e79f5c4093e223d139
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-09-29 03:03:33 +0100

    Solved More Exercises
    More solutions to Tables Relations

--- PostgreSQL/05_Table Relations Exercises/table_relations_all_exercises.sql
+++ PostgreSQL/05_Table Relations Exercises/table_relations_all_exercises.sql
@@ -142,29 +142,205 @@ VALUES
 
 -- 08. Study Session
 
+CREATE TABLE students(
+  id SERIAL PRIMARY KEY,
+  student_name VARCHAR(30)
+);
 
+INSERT INTO students
+    (student_name)
+VALUES
+    ('Mila'),
+    ('Toni'),
+    ('Ron');
+
+CREATE TABLE exams(
+    id INT GENERATED ALWAYS AS IDENTITY (START WITH 101 INCREMENT 1) PRIMARY KEY,
+    exam_name VARCHAR(30)
+);
+
+INSERT INTO exams
+    (exam_name)
+VALUES
+    ('Python Advanced'),
+    ('Python OOP'),
+    ('PostgreSQL');
+
+CREATE TABLE study_halls(
+    id SERIAL PRIMARY KEY,
+    study_hall_name VARCHAR(30),
+    exam_id INT,
+    CONSTRAINT fk_students_halls_exams
+            FOREIGN KEY (exam_id)
+            REFERENCES exams(id)
+);
+
+INSERT INTO study_halls
+    (study_hall_name, exam_id)
+VALUES
+    ('Open Source Hall', 102),
+    ('Inspiration Hall', 101),
+    ('Creative Hall', 103),
+    ('Masterclass Hall', 103),
+    ('Information Security Hall', 103);
+
+CREATE TABLE students_exams(
+    student_id INT,
+    exam_id INT,
+    CONSTRAINT fk_students_exams_students
+        FOREIGN KEY (student_id)
+        REFERENCES students(id),
+    CONSTRAINT fk_students_exams_exams
+        FOREIGN KEY (exam_id)
+        REFERENCES exams(id)
+);
+
+INSERT INTO students_exams
+    (student_id, exam_id)
+VALUES
+    (1, 101),
+    (1, 102),
+    (2, 101),
+    (3, 103),
+    (2, 102),
+    (2, 103);
 
 -- 10. Online Store
 
+CREATE TABLE cities(
+    id SERIAL PRIMARY KEY,
+    city_name VARCHAR(30)
+);
 
+
+CREATE TABLE customers(
+    id SERIAL PRIMARY KEY,
+    customer_name VARCHAR(50),
+    birthday DATE,
+    city_id INT,
+    CONSTRAINT fk_customers_cities
+        FOREIGN KEY (city_id)
+        REFERENCES cities(id)
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    customer_id INT,
+    CONSTRAINT fk_orders_customers
+        FOREIGN KEY (customer_id)
+        REFERENCES customers(id)
+);
+
+CREATE TABLE item_types(
+    id SERIAL PRIMARY KEY,
+    item_type_name VARCHAR(50)
+);
+
+CREATE TABLE items(
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(30),
+    item_type_id INT,
+    CONSTRAINT fk_items_item_types
+        FOREIGN KEY (item_type_id)
+        REFERENCES item_types(id)
+);
+
+CREATE TABLE order_items(
+    order_id INT,
+    item_id INT,
+    CONSTRAINT pk_order_id_item_id
+        PRIMARY KEY (order_id, item_id),
+    CONSTRAINT fk_order_items_orders
+        FOREIGN KEY (order_id)
+        REFERENCES orders(id),
+    CONSTRAINT fk_order_items_items
+        FOREIGN KEY (item_id)
+        REFERENCES items(id)
+);
 
 -- 11. Delete Cascade
+
+ALTER TABLE countries
+ADD CONSTRAINT fk_countries_continents
+    FOREIGN KEY (continent_code)
+    REFERENCES continents(continent_code) ON DELETE CASCADE,
+ADD CONSTRAINT fk_countries_currencies
+    FOREIGN KEY (currency_code)
+    REFERENCES currencies(currency_code) ON DELETE CASCADE;
 
 
 
 -- 12. Update Cascade
 
+ALTER TABLE countries_rivers
+ADD CONSTRAINT fk_countries_rivers_rivers
+    FOREIGN KEY (river_id)
+    REFERENCES rivers(id) ON UPDATE CASCADE,
+ADD CONSTRAINT fk_countries_rivers_countries
+    FOREIGN KEY (country_code)
+    REFERENCES countries(country_code) ON UPDATE CASCADE
+;
 
 
 -- 13. SET NULL
 
+CREATE TABLE IF NOT EXISTS customers(
+    id SERIAL PRIMARY KEY,
+    customer_name VARCHAR(40)
+);
 
+CREATE TABLE IF NOT EXISTS contacts(
+    id SERIAL PRIMARY KEY,
+    contact_name VARCHAR(40),
+    phone VARCHAR(30),
+    email VARCHAR(50),
+    customer_id INT,
+    CONSTRAINT fk_contacts_customers
+            FOREIGN KEY (customer_id)
+            REFERENCES customers(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+INSERT INTO customers (customer_name)
+VALUES
+    ('BlueBird Inc'),
+    ('Dolphin LLC');
+
+INSERT INTO contacts (contact_name, phone, email, customer_id)
+VALUES
+    ('John Doe', '(408)-111-1234', 'john.doe@bluebird.dev', 1),
+    ('Jane Doe', '(408)-111-1235', 'jane.doe@bluebird.dev', 1),
+    ('David Wright', '(408)-222-1234', 'david.wright@dolphin.dev', 2);
+
+DELETE FROM customers
+WHERE id = 1;
 
 -- 14. Peaks in Rila✶
 
+SELECT
+    m.mountain_range,
+    p.peak_name AS peak_name,
+    p.elevation AS elevation
+FROM
+    mountains AS m
+JOIN
+    peaks AS p ON m.id = p.mountain_id
+WHERE
+    m.mountain_range LIKE('Rila')
+ORDER BY
+    elevation DESC;
 
 
 -- 15. Countries Without Any Rivers✶
 
-
+SELECT
+    COUNT(*) as countries_without_rivers
+FROM
+    countries as c
+LEFT JOIN
+    countries_rivers as cr ON c.country_code = cr.country_code
+WHERE
+    cr.river_id IS NULL
+GROUP BY
+    cr.river_id
+    ;
 
--- test.py
+++
@@ -1,7 +0,0 @@
-list_1 = [1, 2, 3]
-list_2 = [7, 8, 9]
-
-for list_1[-1] in list_2:
-    pass
-
-print(list_1)
