ba9d18f361818b526fb9efd8322c6bce44033a8b
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-10-02 15:54:37 +0100

    Solutions Added

--- PostgreSQL/06-Subqueries and JOINs Exercises/subqueries_and_joins_exercises.sql
+++ PostgreSQL/06-Subqueries and JOINs Exercises/subqueries_and_joins_exercises.sql
@@ -140,37 +140,181 @@ ORDER BY elevation DESC
 
 -- 12. Count Mountain Ranges
 
+SELECT
+    mc.country_code,
+    COUNT(*) AS mountain_range_count
+FROM
+    mountains AS m
+JOIN
+    mountains_countries as mc on mc.mountain_id = m.id
+WHERE
+    country_code IN ('RU', 'BG', 'US')
+GROUP BY
+    mc.country_code
+ORDER BY
+    mountain_range_count DESC
+;
 
 
 
 -- 13. Rivers in Africa
 
+SELECT
+    c.country_name,
+    r.river_name
+FROM
+    countries AS c
+LEFT JOIN
+    countries_rivers AS cr ON cr.country_code = c.country_code
+LEFT JOIN
+    rivers AS r ON r.id = cr.river_id
+WHERE
+    c.continent_code = 'AF'
+ORDER BY
+    c.country_name ASC
+LIMIT 5;
 
 
 
 -- 14. Minimum Average Area Across Continents
 
-
+SELECT
+    MIN(average_area) AS min_average_area -- USE THE CREATED COLUMN, NOT THE WHOLE SELECT STATEMENT (average_area) NOT (average)
+FROM
+    (
+        SELECT
+            AVG(area_in_sq_km) AS average_area  -- CREATE THE COL IN THE SELECT STATEMENT
+        FROM
+            countries
+        GROUP BY
+            continent_code
+    ) AS average
 
 
 -- 15. Countries Without Any Mountains
 
-
+SELECT
+    COUNT(*)
+FROM
+    countries AS c
+LEFT JOIN
+    mountains_countries AS mc USING (country_code)
+WHERE
+    mountain_id IS NULL;
 
 
 -- 16. Monasteries by Country✶
 
+CREATE TABLE IF NOT EXISTS monasteries (
+    id SERIAL PRIMARY KEY,
+    monastery_name VARCHAR(255),
+    country_code CHAR(2)
+);
 
+INSERT INTO monasteries
+    (monastery_name, country_code)
+VALUES
+    ('Rila Monastery ''St. Ivan of Rila', 'BG'),
+    ('Bachkovo Monastery ''Virgin Mary"', 'BG'),
+    ('Troyan Monastery ''Holy Mother''s Assumption', 'BG'),
+    ('Kopan Monastery', 'NP'),
+    ('Thrangu Tashi Yangtse Monastery', 'NP'),
+    ('Shechen Tennyi Dargyeling Monastery', 'NP'),
+    ('Benchen Monastery', 'NP'),
+    ('Southern Shaolin Monastery', 'CN'),
+    ('Dabei Monastery', 'CN'),
+    ('Wa Sau Toi', 'CN'),
+    ('Lhunshigyia Monastery', 'CN'),
+    ('Rakya Monastery', 'CN'),
+    ('Monasteries of Meteora', 'GR'),
+    ('The Holy Monastery of Stavronikita', 'GR'),
+    ('Taung Kalat Monastery', 'MM'),
+    ('Pa-Auk Forest Monastery', 'MM'),
+    ('Taktsang Palphug Monastery', 'BT'),
+    ('Sümela Monastery', 'TR');
+
+ALTER TABLE
+    monasteries
+ADD COLUMN
+    three_rivers BOOL DEFAULT FALSE;
+
+UPDATE monasteries
+SET three_rivers = TRUE
+FROM
+    (SELECT
+        country_code
+     FROM
+         countries_rivers
+    GROUP BY
+        country_code
+    HAVING
+        COUNT(*) > 3) AS results
+WHERE
+    monasteries.country_code = results.country_code;
+
+SELECT
+    monastery_name AS "Monastery",
+    country_name AS "Country"
+FROM
+    monasteries
+JOIN
+    countries USING (country_code)
+WHERE
+    three_rivers IS NOT  TRUE
+ORDER BY
+    monastery_name
+;
 
 -- 17. Monasteries by Continents and Countries✶
 
+UPDATE countries
+SET country_name = 'Burma'
+WHERE country_name = 'Myanmar';
 
+INSERT INTO monasteries
+    (monastery_name, country_code)
+VALUES
+    ('Hanga Abbey', (
+        SELECT country_code
+        FROM countries
+        WHERE country_name = 'Tanzania'
+        )),
+    ('Myin-Tin-Daik', (
+        SELECT country_code
+        FROM countries
+        WHERE country_name = 'Myanmar'
+        ));
+
+
+SELECT
+    continents.continent_name AS "Continent Name",
+    countries.country_name AS "Country Name",
+    COUNT(monasteries.monastery_name) AS "Monasteries Count"
+FROM
+    continents
+LEFT JOIN
+    countries USING (continent_code)
+LEFT JOIN
+    monasteries USING (country_code)
+WHERE
+    three_rivers = FALSE OR three_rivers IS NULL
+GROUP BY
+    continent_name, country_name
+ORDER BY
+    "Monasteries Count" DESC, countries.country_name ASC;
 
 
 -- 18. Retrieving Information about Indexes
 
-
-
+SELECT
+    tablename,
+    indexname,
+    indexdef
+FROM
+    pg_indexes
+WHERE schemaname ='public'
+ORDER BY
+    indexname ASC;
 
 -- 19. Continents and Currencies✶
 
