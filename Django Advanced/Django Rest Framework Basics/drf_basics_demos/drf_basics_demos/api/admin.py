143513bd22ae1852cb690e658e40fbd1c081ff03
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-03-18 20:04:29 +0000

    DRF
    Code from the advanced lecture of Django Rest Framework added

--- Django Advanced/Django Rest Framework Basics/drf_basics_demos/drf_basics_demos/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from drf_basics_demos.api.models import Book
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "genre")
