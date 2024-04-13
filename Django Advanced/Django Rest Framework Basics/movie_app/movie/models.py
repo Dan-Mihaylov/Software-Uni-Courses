a5e5dc80c481dc5884764ab7b82eaa591bcdfe5b
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-04-13 23:51:46 +0100

    Exercised more with Serializers
    Done a few more serializers

--- Django Advanced/Django Rest Framework Basics/movie_app/movie/models.py
+++ Django Advanced/Django Rest Framework Basics/movie_app/movie/models.py
@@ -1,20 +1,17 @@
 from django.db import models
 
 
+class Director(models.Model):
+
+    name = models.CharField(max_length=100)
+
+
 class Movie(models.Model):
 
-    title = models.CharField(
-        max_length=100
-    )
+    title = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
-
-    genre = models.CharField(
-        max_length=100
-    )
-
-    director = models.CharField(
-        max_length=100
-    )
+    genre = models.CharField(max_length=100)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title} - {self.year}'
--- Django Advanced/Django Rest Framework Basics/movie_app/movie_api/serializers.py
+++ Django Advanced/Django Rest Framework Basics/movie_app/movie_api/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from movie_app.movie.models import Movie
+from movie_app.movie.models import Movie, Director
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = ['name']
 
 
 class MovieSerializer(serializers.ModelSerializer):
+
+    director = DirectorSerializer(many=False)
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ['id', 'title', 'year', 'genre', 'director']
+
+    def create(self, validated_data):
+
+        director_data = validated_data.pop('director', None)
+        director, created = Director.objects.get_or_create(**director_data)
+
+        movie_data = {
+            **validated_data,
+            'director': director,
+        }
+
+        return Movie.objects.create(**movie_data)
